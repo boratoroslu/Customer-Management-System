01fb5ac130dd8fc934d88eb940bbd6f1684a71dc
boratoroslu <boratoroslu@gmail.com>
2024-07-04 22:05:12 +0300

    miniApp
    04.07.24

---
+++ src/core/Database.java
@@ -0,0 +1,40 @@
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    //singleton desing pattern
+
+    private static Database instance = null;
+    private Connection connection = null;
+    private final String DB_URL = "jdbc:mysql://localhost:3306/customermanage";
+    private final String DB_USERNAME = "root";
+    private final String DB_PASSWORD = "000000";
+
+
+
+    private Database(){
+        try {
+           this.connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private Connection getConnection() {
+        return connection;
+    }
+    public static Connection getInstance(){
+        try {
+            if(instance == null|| instance.getConnection().isClosed()){
+                instance = new Database();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return instance.getConnection();
+    }
+}
---
+++ src/core/Helper.java
@@ -0,0 +1,73 @@
+package core;
+
+import javax.swing.*;
+
+public class Helper {
+    public static void setTheme(){
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if (info.getName().equals("Nimbus")) {
+                try {
+                    UIManager.setLookAndFeel(info.getClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                         UnsupportedLookAndFeelException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            }
+        }
+    }
+    public static boolean isFieldEmpty(JTextField field){
+        return field.getText().trim().isEmpty();
+    }
+    public static boolean isFieldListEmpty(JTextField[] fields ){
+        for(JTextField field : fields){
+            if(isFieldEmpty(field)) return true;
+        }
+        return false;
+    }
+    public static boolean isEmailValid(String mail){
+        // info@gmail.com
+        //@ olmalı , @'den önce bir değer olmalı , @'den sonra nokta (.) ile devam etmeli.
+        //noktadan sonra değer ile devam etmeli
+        if (mail==null || mail.trim().isEmpty()) return false;
+        if (!mail.contains("@")) return false; //Contains string içinde istediğim karakter var mı diye kontrol eder.
+        //mail için @ işareti yok ise false değer döndürür.
+
+        String[] parts = mail.split("@");// girilen stringde @ işaretini bulucak ve ikiye bölecek
+                                                // örn bora@gmail.com  --->  bora | gmail.com
+        if (parts.length != 2) return false;
+
+        if (parts[0].trim().isEmpty() || parts [1].trim().isEmpty()) return false;
+
+        if (!parts[1].contains(".")) return false;
+        return true;
+    }
+    public  static void optionPanrDialogTR(){
+        UIManager.put("OptionPane.okButtonText", "Tamam");
+    }
+
+    public static void showMsg(String message){
+       String msg;
+       String title;
+
+   optionPanrDialogTR();
+        switch (message){
+            case "fill":
+                msg = "Lütfen tüm alanları doldurunuz!";
+                title ="HATA!";
+                break;
+            case "done":
+                msg = "İşlem Başarılı";
+                title ="Sonuç";
+                break;
+            case "error":
+                msg = "Bir hata oluştu!";
+                title ="HATA!";
+                break;
+            default:
+                msg = message;
+                title ="Mesaj";
+        }
+        JOptionPane.showMessageDialog(null, msg,title,JOptionPane.INFORMATION_MESSAGE);
+    }
+}
